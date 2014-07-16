48f2f8e35793d9048dfb7c5f2a3786eecf1ac012
micahjohnson150 <micah.johnson150@gmail.com>
2014-07-16 10:31:04 -0600

    pre- spatial scaling

--- src/ics/ChemicalPotentialIC.C
+++ src/ics/ChemicalPotentialIC.C
@@ -27,5 +27,5 @@ ChemicalPotentialIC::value(const Point & /*p*/)
    return _property_uo.equilibriumConcentration(_temperature[_qp]);
   else
     return 0.0;*/
-  return _property_uo.equilibriumConcentration(_temperature[_qp]);
+  return _property_uo.equilibriumConcentration(_temperature[_qp]) * ((1.0-_phase[_qp])/2.0);
 }
